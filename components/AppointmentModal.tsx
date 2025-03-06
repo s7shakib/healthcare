442561adadbe1536c756c8344a00a7104337e9a9
Mohammad Sultani <132396736+s7shakib@users.noreply.github.com>
2025-03-06 11:57:04 -0500

    Update AppointmentModal.tsx

--- components/AppointmentModal.tsx
+++ components/AppointmentModal.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
-  DialogGontent,
+  DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
